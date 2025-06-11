8bfd8d1b90ddc18297ff1ba6ce89f87b9767f904
Dr Chibuzor Christopher Nnanatu <94153331+ChibuzorNnanatu@users.noreply.github.com>
2025-06-11 13:55:39 +0100

    spices.R

--- R/spices.R
+++ R/spices.R
@@ -141,7 +141,8 @@ spices <-function(df, output_dir, class)# disaggregates by age only - with covar
 
   # join all data
   full_dat <- cbind(df,
-                    pred_dt, pred_dtL,pred_dtU) # everything
+                    prop_dt, prop_dtL,prop_dtU,
+                   pred_dt, pred_dtL,pred_dtU) # everything
 
   # save the datasets
   write.csv(full_dat, paste0(output_dir,"/full_disaggregated_data.csv"),row.names = F)
