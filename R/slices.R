39c9c970614d0e1e7ca4a16334d841bca009caaf
Dr Chibuzor Christopher Nnanatu <94153331+ChibuzorNnanatu@users.noreply.github.com>
2025-06-11 15:09:06 +0100

    slices.R

--- R/slices.R
+++ R/slices.R
@@ -138,7 +138,7 @@ slices <-function(df,output_dir, class)# disaggregates by age only - no covariat
   # join all data
   full_dat <- cbind(df,
                      prop_dt, prop_dtL,prop_dtU,
-                   pred_dt, pred_dtL,pred_dtU)) # everything
+                   pred_dt, pred_dtL,pred_dtU) # everything
 
   # save the datasets
   write.csv(full_dat, paste0(output_dir,"/full_disaggregated_data.csv"),row.names = F)
