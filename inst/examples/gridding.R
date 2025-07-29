fbab7353ca0283d4143165ed5fe73637c85adb6d
Mohamed Yusuf <mohamedayusuf87@gmail.com>
2025-07-29 20:21:37 +0300

    remove toy_grid_data

--- inst/examples/gridding.R
+++ inst/examples/gridding.R
@@ -1,92 +1,90 @@
-# install.packages("devtools")
-#devtools::install_github("wpgp/jollofR")
+# Example script for grid-level population disaggregation
+# Uses toydata included with jollofR package
+
 library(INLA)
 library(tidyverse)
 library(jollofR)
 
-# set directories
-path <- "C:/Users/ccn1r22/OneDrive - University of Southampton/Documents/packages/main/jollofR_scripts"
-data_path <- paste0(path, "/data")
-out_path <- paste0(path, "/output")
+# Load toydata from package
+data(toydata)
 
-# Load key packages and libraries
-# install.packages("jollofR")
+# Extract admin and grid data
+datf <- toydata$admin
+rdatf <- toydata$grid
 
-source(paste0(path, "/sprinkle.R")) #disaggregates population based on the grid pop count
-source(paste0(path, "/splash.R")) #disaggregates population based on the grid building count
-source(paste0(path, "/spray.R")) #disaggregates population by area-weighting
+# Set output directory (creates temp directory for this example)
+out_path <- tempdir()
 
+# Run cheesecake for administrative level disaggregation
+result <- cheesecake(df = datf, output_dir = out_path)
+pyramid(result$fem_age_pop, result$male_age_pop)
 
-# load in put data
-dim(datf <- read.csv(paste0(data_path, "/toy_admin_data.csv")))
-dim(rdatf <- readRDS(paste0(data_path, "/toy_grid_data.rds")))
-
-# run cheesepop
-result <- cheesecake(df = datf,out_path) 
-#result <- cheesecake(df = datf,out_path) 
-pyramid(result$fem_age_pop,result$male_age_pop)
+# Get full data with predictions for grid disaggregation
 dat <- result$full_data
-dat$admin_id <- dat$clst_id
 
-# grid population classes (you can create yours but has to be same as in the admin data)
-rclass <- paste0("TOY_population_v1_0_age",1:12) # Mean
+# Age classes for grid disaggregation (matching admin data structure)
+rclass <- names(datf)[grepl("^age_", names(datf))]
 
-
-# run sprinkle
-output_dir <- paste0(out_path, "/raster/sprinkle")
-result2 <- sprinkle(df = dat, rdf = rdatf, rclass, output_dir) 
+# Run sprinkle (uses known grid cell population totals)
+output_dir <- file.path(out_path, "raster", "sprinkle")
+result2 <- sprinkle(df = dat, rdf = rdatf, rclass, output_dir)
 dev.off()
-pyr2 <- pyramid(result2$fem_age_pop,result2$male_age_pop)
-#result2$full_data
-ras2<- raster(paste0(output_dir, "/pop_TOY_population_v1_0_agesex_f4.tif"))
+pyr2 <- pyramid(result2$fem_age_pop, result2$male_age_pop)
+
+# Plot raster output
+ras2 <- raster(paste0(output_dir, "/pop_", rclass[4], "_f.tif"))
 p2 <- plot(ras2,
-     legend=T, col = colorRampPalette(c("white", "orange", "brown"))(255),
-     asp = NA, cex.axis=3)
+     legend = TRUE, col = colorRampPalette(c("white", "orange", "brown"))(255),
+     asp = NA, cex.axis = 3)
 
-
-# run splash
-output_dir3 <- paste0(out_path, "/raster/splash")
-result3 <- splash(df = dat, rdf = rdatf, rclass, output_dir3) 
+# Run splash (uses building counts as weighting layer)
+output_dir3 <- file.path(out_path, "raster", "splash")
+result3 <- splash(df = dat, rdf = rdatf, rclass, output_dir3)
 dev.off()
-pyr3 <- pyramid(result3$fem_age_pop,result3$male_age_pop)
-#result2$full_data
-ras3<- raster(paste0(output_dir3, "/pop_TOY_population_v1_0_agesex_f4.tif"))
+pyr3 <- pyramid(result3$fem_age_pop, result3$male_age_pop)
+
+# Plot raster output
+ras3 <- raster(paste0(output_dir3, "/pop_", rclass[4], "_f.tif"))
 p3 <- plot(ras3,
-     legend=T, col = colorRampPalette(c("white", "orange", "brown"))(255),
-     asp = NA, cex.axis=3)
+     legend = TRUE, col = colorRampPalette(c("white", "orange", "brown"))(255),
+     asp = NA, cex.axis = 3)
 
-
-# run spray
-output_dir4 <- paste0(out_path, "/raster/spray")
-result4 <- spray(df = dat, rdf = rdatf, rclass, output_dir4) 
+# Run spray (equal weight distribution across grid cells)
+output_dir4 <- file.path(out_path, "raster", "spray")
+result4 <- spray(df = dat, rdf = rdatf, rclass, output_dir4)
 dev.off()
-pyr4 <- pyramid(result4$fem_age_pop,result4$male_age_pop)
-#result2$full_data
-ras4<- raster(paste0(output_dir4, "/pop_TOY_population_v1_0_agesex_f4.tif"))
+pyr4 <- pyramid(result4$fem_age_pop, result4$male_age_pop)
+
+# Plot raster output
+ras4 <- raster(paste0(output_dir4, "/pop_", rclass[4], "_f.tif"))
 p4 <- plot(ras4,
-     legend=T, col = colorRampPalette(c("white", "orange", "brown"))(255),
-     asp = NA, cex.axis=3)
+     legend = TRUE, col = colorRampPalette(c("white", "orange", "brown"))(255),
+     asp = NA, cex.axis = 3)
 
-par(mfrow=c(2,2))
-p2 <- plot(ras2,
-           legend=T, col = colorRampPalette(c("white", "orange", "brown"))(255),
-           asp = NA, cex.axis=3)
-p3 <- plot(ras3,
-           legend=T, col = colorRampPalette(c("white", "orange", "brown"))(255),
-           asp = NA, cex.axis=3)
-p4 <- plot(ras4,
-           legend=T, col = colorRampPalette(c("white", "orange", "brown"))(255),
-           asp = NA, cex.axis=3)
+# Compare all three methods side by side
+par(mfrow = c(2, 2))
+plot(ras2, main = "Sprinkle Method",
+     legend = TRUE, col = colorRampPalette(c("white", "orange", "brown"))(255),
+     asp = NA, cex.axis = 3)
+plot(ras3, main = "Splash Method",
+     legend = TRUE, col = colorRampPalette(c("white", "orange", "brown"))(255),
+     asp = NA, cex.axis = 3)
+plot(ras4, main = "Spray Method",
+     legend = TRUE, col = colorRampPalette(c("white", "orange", "brown"))(255),
+     asp = NA, cex.axis = 3)
 
-
-## plot pyramids together
+# Plot population pyramids together
 library(ggpubr)
 ggarrange(pyr2, pyr3, pyr4,
-          ncol=2, nrow=2)
+          labels = c("Sprinkle", "Splash", "Spray"),
+          ncol = 2, nrow = 2)
 
-
-# compare
-ii <- sample(12,1)
-head(cbind(result2$male_age_pop[, ii], result3$male_age_pop[, ii],result4$male_age_pop[, ii]),30)
-
-
+# Compare results for a random age group
+set.seed(123)  # for reproducibility
+ii <- sample(12, 1)
+comparison <- cbind(
+  Sprinkle = result2$male_age_pop[, ii],
+  Splash = result3$male_age_pop[, ii],
+  Spray = result4$male_age_pop[, ii]
+)
+head(comparison, 30)
